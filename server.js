a5210b634a996d1fa381faa1fd9a3f0a74e8c66e
haydgraham <samuelh.graham@gmail.com>
2023-09-25 17:43:59 -0600

    successfully connected to mongodb database

--- server.js
+++ server.js
@@ -1,12 +1,53 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
+require('dotenv').config();
+const uri = process.env.MONGODB_URI;
 
-const homeRoute = require('./routes/home')
-app.use('/', homeRoute);
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+let contactsCollection; // This will store the reference to the database collection
+
+async function connectToDatabase() {
+    try {
+        await client.connect();
+        console.log("Successfully connected to MongoDB!");
+
+        const db = client.db("test");  // Accessing the "contacts" database
+        contactsCollection = db.collection("contacts"); // Accessing the "Contacts" collection
+
+    } catch (err) {
+        console.error("Error connecting to MongoDB:", err);
+        process.exit(1); // Exit the process with an error code
+    }
+}
+
+// Start the database connection before starting the server
+connectToDatabase().then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost:${port}`);
+    });
+}).catch(err => {
+    console.error("Failed to start server:", err);
+    process.exit(1);
+});
+
+// Define the route to fetch contacts
+app.get('/contacts', async (req, res) => {
+    try {
+        const contacts = await contactsCollection.find().toArray();
+        res.json(contacts);
+    } catch (err) {
+        console.error("Error fetching contacts:", err);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 });
