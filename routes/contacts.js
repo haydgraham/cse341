f3262e4082ffc166660da3442fff3d73ce1d74ae
haydgraham <samuelh.graham@gmail.com>
2023-09-26 15:58:22 -0600

    added 3 new routes and contact controller funcs

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,8 +3,13 @@ const router = express.Router();
 
 const contactsController = require('../controllers/contacts');
 
+// Lesson 2 routes
 router.get('/', contactsController.getAll);
-
 router.get('/:id', contactsController.getSingle);
+
+// Lesson 3 routes
+router.post('/', contactsController.createContact);
+router.put('/:id', contactsController.updateContact);
+router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
